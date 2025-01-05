6fb7a4a303824a5cd77aea57ab676a91aef032a3
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-05 11:35:30 -0500

    set up NextAuth with github oauth

---
+++ src/app/providers.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { NextUIProvider } from '@nextui-org/react';
+
+interface IProvidersProps {
+	children: React.ReactNode;
+}
+
+export default function Providers({ children }: IProvidersProps) {
+	return <NextUIProvider>{children}</NextUIProvider>;
+}
