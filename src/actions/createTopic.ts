a9ef65bcaf70d32cd8fc2a937132e6e9d861076f
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-05 19:28:39 -0500

    save topic to db

--- src/actions/createTopic.ts
+++ src/actions/createTopic.ts
@@ -1,6 +1,12 @@
 'use server';
 
 import { z } from 'zod';
+import { auth } from '@/auth';
+import type { Topic } from '@prisma/client';
+import { redirect } from 'next/navigation';
+import { db } from '@/db';
+import paths from '@/paths';
+import { revalidatePath } from 'next/cache';
 
 const createTopicSchema = z.object({
 	name: z
@@ -16,6 +22,7 @@ export interface ICreateTopicFormState {
 	errors: {
 		name?: string[];
 		description?: string[];
+		_form?: string[];
 	};
 }
 
@@ -34,9 +41,39 @@ export async function createTopic(
 		};
 	}
 
-	return {
-		errors: {}
-	};
+	const session = await auth();
+	if (!session || !session.user) {
+		return {
+			errors: {
+				_form: ['You must be signed in to do this']
+			}
+		};
+	}
 
-	// todo: revalidate the home page
+	let topic: Topic;
+	try {
+		topic = await db.topic.create({
+			data: {
+				slug: result.data.name,
+				description: result.data.description
+			}
+		});
+	} catch (err: unknown) {
+		if (err instanceof Error) {
+			return {
+				errors: {
+					_form: [err.message]
+				}
+			};
+		} else {
+			return {
+				errors: {
+					_form: ['Something went wrong']
+				}
+			};
+		}
+	}
+
+	revalidatePath('/');
+	redirect(paths.topicShowPath(topic.slug));
 }
